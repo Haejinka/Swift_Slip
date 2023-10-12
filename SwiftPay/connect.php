b7c764ab4ee5768af77a0582b6ff726f4255b5f0
MatchaLatte030 <vergaraevon@gmail.com>
2023-10-13 01:03:57 +0800

    minor revisions

--- SwiftPay/connect.php
+++ SwiftPay/connect.php
@@ -3,7 +3,7 @@
 $hostName = "localhost";
 $hostUsername = "root";
 $hostPassword = "";
-$hostDB = "swiift_slip"; //change this depending on you dbname
+$hostDB = "swift_slip"; //change this depending on you dbname
 
 // Connect PHP file to database
 $con = mysqli_connect($hostName, $hostUsername, $hostPassword, $hostDB) or die("Error in Database connection...");
